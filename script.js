93c27eb3fcc75c458bbf0171fe16fdbcd75be99e
Yahoodeev <yg199312012@yahoo.com>
2022-02-22 17:06:19 +0800

    button added

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-var init = function(){  
-  var ps = new ParticleSlider({
-    width: 1e9,
-    height: 1e9,
-    color: 'rgba(255,255,255,1)',
-    ptlGap:1,
-    mouseForce: 100
-  });
+var ps = new ParticleSlider({
+  ptlGap: 1 ,
+  mouseForce: 100 ,
+  monochrome: true ,
+  color: '#fff' ,
+  ptlSize: 1 ,
   
+});
 var ptl = new ps.Particle(ps);
-ptl.color = [255,255,255,255];
-}
-
-var initParticleSlider = function(){
-  var psScript = document.createElement('script');
-  (psScript.addEventListener
-    ? psScript.addEventListener('load', init, false)
-    : psScript.onload = init);
-  psScript.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/23500/ps-0.9.js';
-	psScript.setAttribute('type', 'text/javascript');
-  document.body.appendChild(psScript);
-}
-    
-(window.addEventListener
-  ? window.addEventListener('load', initParticleSlider, false)
-  : window.onload = initParticleSlider);
