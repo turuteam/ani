fe8d2a8f28ce09f6c46fdf3c95918829d95e02ef
Yahoodeev <yg199312012@yahoo.com>
2022-02-22 18:56:15 +0800

    center button

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-var ps = new ParticleSlider({
-  ptlGap: 1 ,
-  mouseForce: 100 ,
-  monochrome: true ,
-  color: '#fff' ,
-  ptlSize: 1 ,
+// var ps = new ParticleSlider({
+//   ptlGap: 1 ,
+//   mouseForce: 100 ,
+//   monochrome: true ,
+//   color: '#fff' ,
+//   ptlSize: 1 ,
   
-});
-var ptl = new ps.Particle(ps);
+// });
+// var ptl = new ps.Particle(ps);
+
+// Tamino Martinius - All rights reserved
+
+// Copyright © 2013 Tamino Martinius (http://zaku.eu)
+// Copyright © 2013 Particleslider.com (http://particleslider.com
+
+// Terms of usage: http://particleslider.com/legal/license
+
+var init = function(){
+  var isMobile = navigator.userAgent &&
+    navigator.userAgent.toLowerCase().indexOf('mobile') >= 0;
+  var isSmall = window.innerWidth < 600;
+  
+  var ps = new ParticleSlider({
+    ptlGap: isMobile || isSmall ? 3 : 0,
+    ptlSize: isMobile || isSmall ? 3 : 1,
+    width: 1e9,
+    height: 1e9,
+    mouseForce: 100 ,
+    color: '#fff',
+    monochrome: true ,
+  });
+}
+
+var initParticleSlider = function(){
+  var psScript = document.createElement('script');
+  (psScript.addEventListener
+    ? psScript.addEventListener('load', init, false)
+    : psScript.onload = init);
+  psScript.src = 'http://particleslider.com/js/particleslider/current/particleslider.js';
+	psScript.setAttribute('type', 'text/javascript');
+  document.body.appendChild(psScript);
+}
+    
+(window.addEventListener
+  ? window.addEventListener('load', initParticleSlider, false)
+  : window.onload = initParticleSlider);
