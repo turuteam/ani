3611d89e5d2f11b04ad45f657b0f1ea56285c46e
Yahoodeev <yg199312012@yahoo.com>
2022-02-22 21:12:51 +0800

    particle background

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-// var ps = new ParticleSlider({
-//   ptlGap: 1 ,
-//   mouseForce: 100 ,
-//   monochrome: true ,
-//   color: '#fff' ,
-//   ptlSize: 1 ,
-  
-// });
+var init = function(){
+  var ps = new ParticleSlider({
+    ptlGap: 1 ,
+    mouseForce: 100 ,
+    monochrome: true ,
+    color: '#fff' ,
+    ptlSize: 1 ,
+    
+  });
+}
+
 // var ptl = new ps.Particle(ps);
 
 // Tamino Martinius - All rights reserved
@@ -15,35 +18,35 @@
 
 // Terms of usage: http://particleslider.com/legal/license
 
-var init = function(){
-  var isMobile = navigator.userAgent &&
-    navigator.userAgent.toLowerCase().indexOf('mobile') >= 0;
-  var isSmall = window.innerWidth < 600;
+// var init = function(){
+//   var isMobile = navigator.userAgent &&
+//     navigator.userAgent.toLowerCase().indexOf('mobile') >= 0;
+//   var isSmall = window.innerWidth < 600;
   
-  var ps = new ParticleSlider({
-    ptlGap: isMobile || isSmall ? 3 : 0,
-    ptlSize: isMobile || isSmall ? 3 : 1,
-    width: 1e9,
-    height: 1e9,
-    mouseForce: 100 ,
-    color: '#fff',
-    monochrome: true ,
-  });
-}
+//   var ps = new ParticleSlider({
+//     ptlGap: isMobile || isSmall ? 3 : 0,
+//     ptlSize: isMobile || isSmall ? 3 : 1,
+//     width: 1e9,
+//     height: 1e9,
+//     mouseForce: 100 ,
+//     color: '#fff',
+//     monochrome: true ,
+//   });
+// }
 
-var initParticleSlider = function(){
-  var psScript = document.createElement('script');
-  (psScript.addEventListener
-    ? psScript.addEventListener('load', init, false)
-    : psScript.onload = init);
-  psScript.src = 'http://particleslider.com/js/particleslider/current/particleslider.js';
-	psScript.setAttribute('type', 'text/javascript');
-  document.body.appendChild(psScript);
-}
+// var initParticleSlider = function(){
+//   var psScript = document.createElement('script');
+//   (psScript.addEventListener
+//     ? psScript.addEventListener('load', init, false)
+//     : psScript.onload = init);
+//   psScript.src = 'http://particleslider.com/js/particleslider/current/particleslider.js';
+// 	psScript.setAttribute('type', 'text/javascript');
+//   document.body.appendChild(psScript);
+// }
     
 (window.addEventListener
-  ? window.addEventListener('load', initParticleSlider, false)
-  : window.onload = initParticleSlider);
+  ? window.addEventListener('load', init, false)
+  : window.onload = init);
 
 
   /* ---- particles.js config ---- */
